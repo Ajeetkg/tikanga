ea5f540131aaffef38229f92164e052b5b285058
Viaan Gupta <viaangupta@Viaans-MacBook.local>
2023-05-29 06:53:03 -0700

    firebase auth added

---
+++ lib/screens/mobile_layout_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MobileLayoutScreen extends StatelessWidget {
+  const MobileLayoutScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Welcome to tikanga"),
+      ),
+    );
+  }
+}
